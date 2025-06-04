4d265aafb3e1ac0b3e47254501a9381b68675965
Jaime Aranaz <jaime.aranaz@gmail.com>
2025-06-04 10:12:00 +0200

    Add new modules for layer and inject all elementos from App

---
+++ NewBrastlewark/Packages/Data/Sources/Data/DataModule.swift
@@ -0,0 +1,29 @@
+import Domain
+import Swinject
+
+public struct DataModule {
+    public static func registerDependencies(inContainer container: Container) {
+        container.register(NetworkStatusProtocol.self) { _ in NetworkStatus() }
+        container.register(NetworkServiceProtocol.self) { r in
+            guard let networkStatus = r.resolve(NetworkStatusProtocol.self) else {
+                fatalError("Dependency NetworkStatusProtocol not resolved")
+            }
+            return NetworkService(
+                baseUrl: "https://raw.githubusercontent.com/rrafols/mobile_test/master/data.json",
+                networkStatus: networkStatus)
+        }
+        container.register(CharactersCache.self) { _ in
+            InMemoryCharactersCache()
+        }
+        container.register(CharactersRepositoryProtocol.self) { r in
+            guard let networkService = r.resolve(NetworkServiceProtocol.self),
+                  let cache = r.resolve(CharactersCache.self) else {
+                fatalError("Dependencies not resolved")
+            }
+            return CharactersRepository(networkService: networkService, cache: cache)
+        }
+        container.register(FilterRepositoryProtocol.self) { r in
+            FilterRepository()
+        }
+    }
+}
