62146fa7896dae6be5020ce31ca4cd74b6259ddc
Jaime Aranaz <jaime.aranaz@gmail.com>
2025-06-11 13:47:24 +0200

    Use Domain, Data and Presentation with App, and fix errors with SPM

--- NewBrastlewark/Packages/Data/Package.swift
+++ NewBrastlewark/Packages/Data/Package.swift
@@ -15,16 +15,15 @@ let package = Package(
             targets: ["Data"]),
     ],
     dependencies: [
-        .package(path: "../Domain")
-//        .package(url: "https://github.com/Swinject/Swinject.git", from: "2.9.1")
+        .package(path: "../Domain"),
+        .package(url: "https://github.com/Swinject/Swinject.git", from: "2.9.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "Data",
-            dependencies: ["Domain"],
-//            dependencies: ["Domain", "Swinject"],
+            dependencies: ["Domain", "Swinject"],
             resources: [
                 .process("Resources")
             ]),
